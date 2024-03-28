a36f275359c0c3bb1d3b19f7d67974295a7ca447
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-03-29 05:29:22 +0530

    define service classes

---
+++ api/services/ApplicationService.ts
@@ -0,0 +1,8 @@
+import Application from "../models/Application";
+import { ApiBaseService } from "./ApiBaseService";
+
+export default class ApplicationService extends ApiBaseService {
+  async info(): Promise<Application> {
+    return await this.call<Application>("/");
+  }
+}
