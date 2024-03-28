fb4ae1c775cd74d115ee5d9000d42aceae1d9421
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-03-28 06:47:49 +0530

    setup configurations for api and client side

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,4 +2,13 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   extends: ["./api"],
-})
+
+  runtimeConfig: {
+    public: {
+      baseUrl: "http://localhost:3000",
+      homeUrl: "/dashboard",
+      loginUrl: "/login",
+      verificationUrl: "/verify-email",
+    },
+  },
+});
