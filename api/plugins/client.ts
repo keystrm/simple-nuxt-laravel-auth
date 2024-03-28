9eb7dfb447486da113b3d41f17a64b945e083b21
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-03-28 07:04:00 +0530

    define fetch options to create instance of a client

--- api/plugins/client.ts
+++ api/plugins/client.ts
@@ -1,6 +1,31 @@
+import { type FetchOptions } from 'ofetch';
+
 export default defineNuxtPlugin((nuxtApp) => {
     const event = useRequestEvent();
     const config = useRuntimeConfig();
     const user = useUser();
     const apiConfig = config.public.api;
+
+    const httpOptions: FetchOptions = {
+        baseURL: apiConfig.baseUrl,
+        credentials: 'include',
+        headers: {
+            Accept: 'application/json',
+        },
+        retry: false,
+
+        onRequest({ options }) {
+            // TODO
+        },
+
+        onResponse({ response }) {
+            // TODO
+        },
+
+        onResponseError({ response }) {
+            // TODO
+        },
+    };
+
+    const client: any = $fetch.create(httpOptions);
 });
